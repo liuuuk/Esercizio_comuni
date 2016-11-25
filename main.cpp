3774fd6ff245a6d94d18c6de0a888ffbb7c10eb9
liuuuk <l.palonca@campus.unimib.it>
2016-11-25 12:17:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
+#include <iostream>
+#include <string>
+#include <sstream> 
+#include <stdlib.h>
+
 std::string file_by_row(std::ifstream &inf){
   std::string st ;
   if(inf.is_open()){
@@ -9,13 +14,34 @@ std::string file_by_row(std::ifstream &inf){
   //nel caso non sia aperta devo tornare una stringa 
 }
 
-std::string string_sep(const std::string &str){
+comune &string_to_comune(const std::string &str){
  
 	std::stringstream ss;
 	std::string s;
+	comune c;
 
- 	ss.str (str);
-	
+ 	ss.str (argv[1]);
 	std::getline(ss, s, ',');
-  return s;
+	c.nome = s;
+
+	std::getline(ss, s, ',');
+	c.ind = s;
+
+	std::getline(ss, s, ',');
+	c.cap = atoi(s.c_str());
+
+	std::getline(ss, s, ',');
+	c.sup = atoi(s.c_str());
+
+	std::getline(ss, s, ',');
+	c.asl = s;
+
+	std::getline(ss, s, ',');
+	c.cod_istat = atoi(s.c_str());
+
+	std::getline(ss, s, ',');
+	c.distanza_mi = atoi(s.c_str());
+	
+
+	return c;
 }
