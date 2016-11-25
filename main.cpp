585ed769031d3137a97ee25dd14768dcc98b6993
SimoPane <s.panepinto@campus.unimib.it>
2016-11-25 11:37:35 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,8 @@
-
+std::string file_by_row(std::ifstream &inf){
+  std::string st ;
+  if(inf.is_open()){
+    std::getline(inf,st);
+    
+  }
+  return st;//nel caso non sia aperta devo tornare una stringa 
+}
